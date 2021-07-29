14917451150cd3cd69695e2e94747d20532436e6
Will <will_constable@msn.com>
2021-07-29 14:49:18 +0100

    Add contact page layout

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="contact">
-    This will be the contact page
+    <h1>Contact Page</h1>
   </div>
 </template>
 
 <style scoped lang="scss">
-
+.contact {
+  min-height: 80rem;
+  background-color: dodgerblue;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
